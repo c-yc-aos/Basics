38475e2bbba8805dff5679caf181561522533e62
雨化作云 <cycaos@163.com>
2020-06-01 14:31:17 +0800

    upfile
    upFile

--- Node/file/file.js
+++ Node/file/file.js
@@ -10,9 +10,10 @@ var _path = {
 	"img"  : "../../Img/",
 }
 var cache = {};
+var cacheOpen = false ;//缓存服务是否开启
 module.exports = {
 	GetFile : function (Type,Absolute,v,func) {
-		if (cache[Type+Absolute+v] && false) return func(null,cache[Type+Absolute+v]);
+		if (cacheOpen && cache[Type+Absolute+v] ) return func(null,cache[Type+Absolute+v]);
 		fs.readFile(path.join(__dirname,_path[Type]+Absolute),function ( err , data ){
 			if (err) {
 				console.log("readFile Error !",_path[Type]+Absolute,err);
@@ -24,7 +25,7 @@ module.exports = {
 		});
 	},
 	GetImage: function (Type,Absolute,v,func) {
-		if (cache[Type+Absolute+v] && false) return func(null,cache[Type+Absolute+v]);
+		if (cacheOpen && cache[Type+Absolute+v] ) return func(null,cache[Type+Absolute+v]);
 		fs.readFile(path.join(__dirname,_path[Type]+Absolute),function ( err , data ){
 			if (err) {
 				console.log("readFile Error !",_path[Type]+Absolute,err);
--- Node/routers/template.js
+++ Node/routers/template.js
@@ -1,4 +1,5 @@
 //Html 模板化驱动文件 
+// 正则表达式  \{\{ .* \}\}
 function package(){
 	
 }
